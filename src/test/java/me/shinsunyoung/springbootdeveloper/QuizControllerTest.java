ad7b1528d1d824e61acdbc3c9ae18b118b82c95f
Younghan Yonn <dudgks1172@naver.com>
2024-11-10 13:55:10 +0900

    Chapter 4 테스트 코드 연습 문제 1

---
+++ src/test/java/me/shinsunyoung/springbootdeveloper/QuizControllerTest.java
@@ -0,0 +1,66 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class QuizControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper; // 객체와 JSON 간의 변환을 처리
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @DisplayName("quiz() : GET /quiz?code=1 이면 응답 코드는 201, 응답 본문은 Created!를 리턴한다.")
+    @Test
+    public void getQuiz1() throws Exception {
+        // given
+        final String url = "/quiz";
+
+        // when
+        final ResultActions result = mockMvc.perform(get(url).param("code", "1"));
+
+        // then
+        result
+                .andExpect(status().isCreated())
+                .andExpect(content().string("created!"));
+    }
+
+    @DisplayName("quiz)_ : GET /quiz?code2 이면 응답 코드는 400, 응답 본문은 BadRequest!를 리턴한다.")
+    @Test
+    public void getQuiz2() throws Exception {
+        // gievn
+        final String url = "/quiz";
+
+        // when
+        final ResultActions result = mockMvc.perform(get(url).param("code", "2"));
+
+        // given
+        result
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("badRequest!"));
+    }
+}
